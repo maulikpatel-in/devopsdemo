b4fbbc1653e6e19eb7e692a8d748992f5956fcd2
priyankkp-1 <priyank@knp-tech.com>
2020-03-15 13:35:10 +0530

    title change 2nd try

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "DevOps Demo Project - 1st try" });
+  res.render("index", { title: "DevOps Demo Project - 2nd try" });
 });
 
 module.exports = router;
